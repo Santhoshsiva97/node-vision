765a945bf08cc5b62c4b38cee3f1974648f05bff
Santhosh Sivakumar <santhoshsiva2409@gmail.com>
2023-03-23 23:28:47 +0530

    api endpoint implementation with aws rekognition API

--- routes/vision.js
+++ routes/vision.js
@@ -1,17 +1,48 @@
 var express = require('express');
+var AWS = require('aws-sdk');
 var router = express.Router();
 
-router.post('/classify', function(req, res, next) {
-  // DON'T return the hardcoded response after implementing the backend
-  let response = ["shoe", "red", "nike"];
 
-  // Your code starts here //
+const access_key = "AKIARAR74F5B2ZJFROOU"
+const secret_key = "58t6FYfBVhi0FhEKFwxOWExsgASY3dtg6EHAPcVP"
 
-  // Your code ends here //
+// Setting the aws credentials
+var credentials = new AWS.SharedIniFileCredentials({ profile: 'temp' });
+credentials.accessKeyId = access_key;
+credentials.secretAccessKey = secret_key;
+AWS.config.credentials = credentials;
+AWS.config.update({
+  region:'us-east-1'
+});
 
+// Route path 
+router.post('/classify', async function(req, res, next) {
+
+  const client = new AWS.Rekognition();
+  const params = {
+    Image: {
+      Bytes: req.files.file.data, // data to be processed
+    },
+    MaxLabels: 10
+  }
+
+  let labelsContent = [];  
+
+  await client.detectLabels(params, function(err, response) {
+    if (err) {
+      console.log(err, err.stack); // if error occurs
+    } else {
+      response.Labels.forEach(label => {
+        labelsContent.push(label.Name);
+      }) 
+    } 
+  }).promise();
+
+  // returns the labels in response
   res.json({
-    "labels": response
+    "labels": labelsContent
   });
+  
 });
 
 module.exports = router;
